6b0bf5b6c6eebe9a3f2231f3f13603f36d1d4441
santy117 <santy_chapela@hotmail.com>
2024-11-27 16:42:12 +0100

    fix circular dependencies

--- infrastructure/src/main/java/org/example/PTSantiago/output/repository/BalanceH2RepositoryImpl.java
+++ infrastructure/src/main/java/org/example/PTSantiago/output/repository/BalanceH2RepositoryImpl.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class BalanceJPARepositoryImpl implements BalanceRepository {
+public class BalanceH2RepositoryImpl implements BalanceRepository {
 
     @Autowired
     BalanceJPARepository balanceJPARepository;
